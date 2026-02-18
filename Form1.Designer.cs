c501a3b40f5aee9c8d497ad94cdacf072d6a5bf1
Nisanur ÇAP <capnisanur33@gmail.com>
2026-02-18 12:28:23 +0300

    client

---
+++ Form1.Designer.cs
@@ -0,0 +1,272 @@
+﻿
+namespace CLIENT
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.ımageList2 = new System.Windows.Forms.ImageList(this.components);
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtSaat = new System.Windows.Forms.TextBox();
+            this.CONNECT = new System.Windows.Forms.GroupBox();
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.ımageList1 = new System.Windows.Forms.ImageList(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPORT = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtIP = new System.Windows.Forms.TextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richtxtSendData = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richtxtReceivedData = new System.Windows.Forms.RichTextBox();
+            this.groupBox2.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.CONNECT.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.btnSend);
+            this.groupBox2.Controls.Add(this.txtSend);
+            this.groupBox2.Location = new System.Drawing.Point(16, 627);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(837, 100);
+            this.groupBox2.TabIndex = 24;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "SEND";
+            // 
+            // btnSend
+            // 
+            this.btnSend.BackColor = System.Drawing.Color.AliceBlue;
+            this.btnSend.ImageKey = "send.png";
+            this.btnSend.ImageList = this.ımageList2;
+            this.btnSend.Location = new System.Drawing.Point(681, 21);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(125, 71);
+            this.btnSend.TabIndex = 11;
+            this.btnSend.UseVisualStyleBackColor = false;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // ımageList2
+            // 
+            this.ımageList2.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList2.ImageStream")));
+            this.ımageList2.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList2.Images.SetKeyName(0, "send.png");
+            // 
+            // txtSend
+            // 
+            this.txtSend.Location = new System.Drawing.Point(26, 45);
+            this.txtSend.Multiline = true;
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(631, 39);
+            this.txtSend.TabIndex = 10;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtSaat);
+            this.groupBox1.Location = new System.Drawing.Point(441, 169);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(412, 267);
+            this.groupBox1.TabIndex = 23;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "BAĞLANTILAR";
+            // 
+            // txtSaat
+            // 
+            this.txtSaat.Location = new System.Drawing.Point(23, 27);
+            this.txtSaat.Multiline = true;
+            this.txtSaat.Name = "txtSaat";
+            this.txtSaat.Size = new System.Drawing.Size(366, 226);
+            this.txtSaat.TabIndex = 13;
+            // 
+            // CONNECT
+            // 
+            this.CONNECT.Controls.Add(this.btnConnect);
+            this.CONNECT.Controls.Add(this.label3);
+            this.CONNECT.Controls.Add(this.txtPORT);
+            this.CONNECT.Controls.Add(this.label4);
+            this.CONNECT.Controls.Add(this.txtIP);
+            this.CONNECT.Location = new System.Drawing.Point(441, 14);
+            this.CONNECT.Name = "CONNECT";
+            this.CONNECT.Size = new System.Drawing.Size(412, 133);
+            this.CONNECT.TabIndex = 22;
+            this.CONNECT.TabStop = false;
+            this.CONNECT.Text = "CONNECT";
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.BackColor = System.Drawing.Color.AliceBlue;
+            this.btnConnect.ImageKey = "connect2.png";
+            this.btnConnect.ImageList = this.ımageList1;
+            this.btnConnect.Location = new System.Drawing.Point(256, 26);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(125, 71);
+            this.btnConnect.TabIndex = 9;
+            this.btnConnect.UseVisualStyleBackColor = false;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // ımageList1
+            // 
+            this.ımageList1.ImageStream = ((System.Windows.Forms.ImageListStreamer)(resources.GetObject("ımageList1.ImageStream")));
+            this.ımageList1.TransparentColor = System.Drawing.Color.Transparent;
+            this.ımageList1.Images.SetKeyName(0, "connect2.png");
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(20, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 17);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "PORT";
+            // 
+            // txtPORT
+            // 
+            this.txtPORT.Location = new System.Drawing.Point(90, 69);
+            this.txtPORT.Name = "txtPORT";
+            this.txtPORT.Size = new System.Drawing.Size(142, 22);
+            this.txtPORT.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(22, 39);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(20, 17);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "IP";
+            // 
+            // txtIP
+            // 
+            this.txtIP.Location = new System.Drawing.Point(90, 39);
+            this.txtIP.Name = "txtIP";
+            this.txtIP.Size = new System.Drawing.Size(142, 22);
+            this.txtIP.TabIndex = 8;
+            this.txtIP.TextChanged += new System.EventHandler(this.txtIP_TextChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CLIENT.Properties.Resources.poyrai;
+            this.pictureBox1.Location = new System.Drawing.Point(471, 445);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(356, 163);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 21;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(27, 315);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 17);
+            this.label2.TabIndex = 20;
+            this.label2.Text = "Send Data :";
+            // 
+            // richtxtSendData
+            // 
+            this.richtxtSendData.Location = new System.Drawing.Point(30, 335);
+            this.richtxtSendData.Name = "richtxtSendData";
+            this.richtxtSendData.Size = new System.Drawing.Size(382, 260);
+            this.richtxtSendData.TabIndex = 19;
+            this.richtxtSendData.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(27, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(101, 17);
+            this.label1.TabIndex = 18;
+            this.label1.Text = "Recieve Data :";
+            // 
+            // richtxtReceivedData
+            // 
+            this.richtxtReceivedData.Location = new System.Drawing.Point(30, 40);
+            this.richtxtReceivedData.Name = "richtxtReceivedData";
+            this.richtxtReceivedData.Size = new System.Drawing.Size(382, 260);
+            this.richtxtReceivedData.TabIndex = 17;
+            this.richtxtReceivedData.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.ClientSize = new System.Drawing.Size(892, 742);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.CONNECT);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richtxtSendData);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richtxtReceivedData);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.CONNECT.ResumeLayout(false);
+            this.CONNECT.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button btnSend;
+        private System.Windows.Forms.ImageList ımageList2;
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtSaat;
+        private System.Windows.Forms.GroupBox CONNECT;
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.ImageList ımageList1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPORT;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtIP;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richtxtSendData;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richtxtReceivedData;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,174 @@
+﻿using SimpleTCP;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+
+
+namespace CLIENT
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            Control.CheckForIllegalCrossThreadCalls = false;
+        }
+
+        SimpleTcpClient client;
+        List<string> ip = new List<string>();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            client = new SimpleTcpClient();
+            client.StringEncoder = Encoding.UTF8;
+            richtxtReceivedData.Enabled = false;
+            richtxtSendData.Enabled = false;
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            //if (txtIP.Text.Length==15)
+            //{
+            //    client.Connect(txtIP.Text, Convert.ToInt32(txtPORT.Text));
+            //    btnConnect.Enabled = false;
+            //    ip.Add(txtIP.Text);
+            //    Baglanti();
+            //}
+            //else
+            //{
+            //    try
+            //    {
+            //        client.Connect("192.168.1.75", 23);
+            //        btnConnect.Enabled = false;
+            //        ip.Add("192.168.1.75");
+            //        Baglanti();
+            //    }
+            //    catch (Exception)
+            //    {
+            //        MessageBox.Show("Server Bulunamadı");
+            //        btnConnect.Enabled = true;
+            //        BaglantiSonrasiRenk();
+            //    }
+            //}
+            client.Connect(txtIP.Text, Convert.ToInt32(txtPORT.Text));
+        }
+
+        private void mesajGonderme()
+        {
+            try
+            {
+                client.WriteLineAndGetReply(txtSend.Text, TimeSpan.FromSeconds(3));
+                richtxtSendData.Text += " -- " + txtSend.Text + "\n";
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Bağlantı Kesildi");
+                btnConnect.Enabled = true;
+                BaglantiSonrasiRenk();
+
+            }
+        }
+
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            int timeoutMilliseconds = 5000; // 5 saniye
+
+            if (txtIP.Text.Length == 15)
+            {
+                using (TcpClient client = new TcpClient())
+                {
+                    IAsyncResult result = client.BeginConnect(txtIP.Text, Convert.ToInt32(txtPORT.Text), null, null);
+                    bool success = result.AsyncWaitHandle.WaitOne(timeoutMilliseconds);
+
+                    if (!success)
+                    {
+                        MessageBox.Show("Sunucuya bağlantı sağlanamadı. Tekrar giriş yapmayı deneyiniz.");
+                        btnConnect.Enabled = true;
+                        txtSend.Clear();
+                        BaglantiSonrasiRenk();
+                    }
+                    else
+                    {
+                        // Bağlantı başarılı.
+                        mesajGonderme();
+                    }
+                }
+            }
+            else
+            {
+                try
+                {
+                    if (true)
+                    {
+                        using (TcpClient client = new TcpClient())
+                        {
+                            IAsyncResult result = client.BeginConnect("192.168.1.75", 23, null, null);
+                            bool success = result.AsyncWaitHandle.WaitOne(timeoutMilliseconds);
+
+                            if (!success)
+                            {
+                                MessageBox.Show("Sunucuya bağlantı sağlanamadı. Tekrar giriş yapmayı deneyiniz.");
+                                btnConnect.Enabled = true;
+                                txtSend.Clear();
+                                BaglantiSonrasiRenk();
+                            }
+                            else
+                            {
+                                // Bağlantı başarılı.
+                                mesajGonderme();
+                            }
+                        }
+                    }
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Server Bulunamadı");
+                    btnConnect.Enabled = true;
+                    BaglantiSonrasiRenk();
+                }
+            }
+
+        }
+
+
+        private void BaglantiSonrasiRenk()
+        {
+            if (ip.Contains("192.168.1.75"))
+            {
+                txtSaat.ForeColor = Color.Red;
+                txtSaat.Text += Environment.NewLine + DateTime.Now.ToLongTimeString() + " -> " + "Bağlantı Kesildi.";
+                ip.Remove("192.168.1.75");
+            }
+            else if (ip.Contains(txtIP.Text))
+            {
+                txtSaat.ForeColor = Color.Red;
+                txtSaat.Text += Environment.NewLine + DateTime.Now.ToLongTimeString() + " -> " + "Bağlantı Kesildi.";
+                ip.Remove(txtIP.Text);
+            }
+        }
+
+        private void Baglanti()
+        {
+            txtSaat.Clear();
+            for (int i = 0; i < ip.Count; i++)
+            {
+                txtSaat.ForeColor = Color.Green;
+                txtSaat.Text += DateTime.Now.ToLongTimeString() + " -> " + ip[i];
+            }
+        }
+
+        private void txtIP_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
